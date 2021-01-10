205c95c0dbf28d26f82f2072375ab3e4400cc789
Jose Luis <jolui334@gmail.com>
2021-01-10 17:45:54 -0600

    File updating attempt 1

---
+++ Python-Challenge.py
@@ -0,0 +1,5 @@
+""" In order to push a commit at git do the next:
+    git status --> gives you a breakdown of what needs to be updated
+    git add . --> prepares the content that will be updated
+    git commit -m "Message here" Commits the changes
+    git push --> Updates the changes at the remote repository"""
